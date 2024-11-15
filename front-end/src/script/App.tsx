685b46c580d7a75a98a5747e0aaea842b525aed5
DoomDayKross <21110777@student.hcmute.edu.vn>
2024-11-15 22:16:47 +0700

    First Doom commit

--- front-end/src/script/App.tsx
+++ front-end/src/script/App.tsx
@@ -1,9 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./login/Login";
 function App() {
-
   return (
-    <>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />}>
+          {" "}
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
